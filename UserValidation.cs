71d7e4c4aea7150cd2af31af0b21a21f64e3e788
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-01 15:03:03 +0530

    Added Rule-3 to enter valid password

--- UserValidation.cs
+++ UserValidation.cs
@@ -40,9 +40,9 @@ namespace UserRegistrationProblem
             return Regex.IsMatch(MobNum, Regex_MobileNumber);
         }
 
-        ////--------Password-Rule-2-------///
+        ////--------Password-Rule-3-------///
 
-        public const string Regex_password = "^[A-Z]{1}[0-9A-Za-z!@#$%^&*?<>]{8,15}$"; 
+        public const string Regex_password = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$"; 
         public bool ValidatePassword(string Password)
         {
             return Regex.IsMatch(Password, Regex_password);
