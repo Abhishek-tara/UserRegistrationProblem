e6aeb0f7fe9ffaacaf24d5a4630e4f695c12f38b
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-01 11:54:02 +0530

    Added method to enter valid First name

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace UserRegistrationProblem
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserRegistrationProblem
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the User Registration Problems using Regular Expression");
+
+            ValidFirstname firstname = new ValidFirstname();
+
+            Console.Write("Enter Your First Name : ");
+
+            string FirstName = Console.ReadLine();
+
+            bool val = firstname.ValidateFirstName(FirstName);
+
+            if (val)
+            {
+                Console.WriteLine("FirstName is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid FirstName");
+            }
         }
     }
 }
